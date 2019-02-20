113d3e60e3ff264dce038997ca24f52d8cbb5ca1
Jordan <jordanhaddadi@gmail.com>
2019-02-20 17:12:25 -0500

    crashing because of _.extend syntax in games.js mongo collection

--- imports/api/collections/games.js
+++ imports/api/collections/games.js
@@ -1,3 +1,40 @@
 import { Mongo } from 'meteor/mongo';
 
 export default Games = new Mongo.Collection('games');
+
+_.extend(Games, {
+
+    newGame() {
+        let gameDoc = {
+            board: [[null, null, null], [null, null, null], [null, null, null]],
+            players: []
+        };
+        //insert a new game into the mongo collection & return the newly created game's id
+        let gameId = Games.insert(gameDoc);
+        return gameId;
+    },
+      
+    //allow users to join by pushing them into the game, which is found with the gameId 
+    //update/set the game with the new user
+    joinGame(gameId, user){
+        let game = Games.findOne(gameId);
+        game.players.push({
+            userId: user._id,
+            username: user.username
+        });
+        Games.update(game._id, {
+            $set: {players: game.players}
+        });
+    },
+
+    //different from findOneandRemove -- we just want to 'reject'/eject users from the current game when they click the leave game button
+    leaveGame(gameId, user){
+        let game = Games.findOne(gameId);
+        game.players = _.reject(game.players, (player) => {
+            return player.userId === user._id;
+        });
+        Games.update(game._id, {
+            $set: {players: game.players}
+        });
+    }
+}) 
--- imports/ui/GameList.jsx
+++ imports/ui/GameList.jsx
@@ -5,10 +5,7 @@ export default class GameList extends Component {
 
     // create a new game
     handleNewGame(){
-        let gameDoc = {
-            board: [[null, null, null] [null, null, null] [null, null, null]]
-        };
-        Games.insert(gameDoc); //insert each new game document into the mongo collection
+        Games.newGame(this.props.user);
     }
 
     // relay the mapped game info back to the enterGameHandler in <App>
@@ -16,14 +13,39 @@ export default class GameList extends Component {
         this.props.enterGameHandler(gameId);
     }
 
+    handleJoinGame(gameId){
+        Games.joinGame(gameId, this.props.user);
+    }
+
+    handleLeaveGame(gameId){
+        Games.leaveGame(gameId, this.props.user);
+    }
+
+    //show each player's username on each game
+    renderPlayers(game){
+        let player1 = game.players.length > 0 ? game.players[0].username: '';
+        let player2 = game.players.length > 1 ? game.players[1].username: ''
+        return (
+            <span>[{player1}] vs [{player2}]</span>
+        )
+    }
+
+    myCurrentGameId(){
+        //find the game that the user is in
+        let game = _.find(this.props.games, (game) => {
+            return _.find(game.players, (player) => {
+                return player.userId === this.props.user._id;
+            })
+             !== undefined;
+        });
+
+        if (game === undefined) return null;
+        return game._id;
+    }
+
     render(){
         return (
             <div>
-                <div>
-                    <button onClick={this.handleNewGame.bind(this)}>
-                        New Game
-                    </button>
-                </div>
                 <div>
                     <h1>Jojo's Game List</h1>
                     {/* map through the list of games, adding a button to each and increasing eaches index by 1/displaying eaches index as the game # */}
@@ -31,12 +53,35 @@ export default class GameList extends Component {
                         return (
                             <div key={game._id}>
                                 <span>Game {index+1}</span>
-                                <button onClick={this.handleEnterGame.bind(this, game._id)}>Enter</button>
+                                {this.renderPlayers(game)}
+
+                            {/* users can only leave if they're in the game room and the game hasn't started */}
+                                {this.myCurrentGameId() === game._id && game.players.length < 2 ? (
+                                    <button onClick={this.handleLeaveGame.bind(this, game._id)}>Leave</button> ) : null}
+
+                                {/* users can only join if they're not in any others games and the game hasn't started */}
+                                {this.myCurrentGameId() === null && game.players.length < 2 ? (
+                                    <button onClick={this.handleJoinGame.bind(this, game._id)}>Join</button>) : null}
+                                
+                                {/* users can only enter if the game is started */}
+                                {game.players.length === 2 ? (
+                                    <button onClick=
+                                    {this.handleEnterGame.bind(this, game._id)}>Enter</button>) : null}
                             </div>
                         )
                     })}
                 </div>
-            </div>
+
+                    {/* only show new game button if user is not in any room  */}
+                    {this.myCurrentGameId() === null ? (
+                        <div>
+                            <button onClick={this.handleNewGame.bind(this)}>
+                                New Game
+                            </button>
+                        </div>
+                    ) : null}
+
+            </div>  
         )
     }
 }
